d8c94777b3e72b210ac86e2b9bf9eec64fab0557
kuro-mujou <mguyenmanhtuan@gmail.com>
2023-09-10 12:08:17 +0700

    fix UI class

--- src/login/LoginPage.java
+++ src/login/LoginPage.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package login;
 
 import java.util.HashMap;
 
-/**
- *
- * @author ADMIN
- */
 public class LoginPage {
     HashMap<String,String> logininfo = new HashMap<String,String>();
     LoginPage(HashMap<String,String> loginInforOriginal)
