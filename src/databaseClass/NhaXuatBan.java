818d403c676b4e6c259f08444e32e567ae75fa40
xuan tho <nguyenxuantho2003@gmail.com>
2023-09-10 21:28:35 +0700

    sữa lại khai báo database và thêm file connectData

--- src/databaseClass/NhaXuatBan.java
+++ src/databaseClass/NhaXuatBan.java
@@ -5,7 +5,64 @@ public class NhaXuatBan
 {
     private int MaNXB;
     private String TenNXB;
-    private String DiaChiNXB;
+    private String DiaChi;
     private String Email;
     private String ThongTinNDD;
+
+    public NhaXuatBan() {
+    }
+
+    public NhaXuatBan(int MaNXB) {
+        this.MaNXB = MaNXB;
+    }
+
+    public NhaXuatBan(int MaNXB, String TenNXB, String DiaChiNXB, String Email, String ThongTinNDD) {
+        this.MaNXB = MaNXB;
+        this.TenNXB = TenNXB;
+        this.DiaChi = DiaChiNXB;
+        this.Email = Email;
+        this.ThongTinNDD = ThongTinNDD;
+    }
+
+    public int getMaNXB() {
+        return MaNXB;
+    }
+
+    public String getTenNXB() {
+        return TenNXB;
+    }
+
+    public String getDiaChiNXB() {
+        return DiaChi;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getThongTinNDD() {
+        return ThongTinNDD;
+    }
+
+    public void setMaNXB(int MaNXB) {
+        this.MaNXB = MaNXB;
+    }
+
+    public void setTenNXB(String TenNXB) {
+        this.TenNXB = TenNXB;
+    }
+
+    public void setDiaChiNXB(String DiaChiNXB) {
+        this.DiaChi = DiaChiNXB;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setThongTinNDD(String ThongTinNDD) {
+        this.ThongTinNDD = ThongTinNDD;
+    }
+    
+    
 }
--- src/databaseClass/TacGia.java
+++ src/databaseClass/TacGia.java
@@ -5,5 +5,46 @@ public class TacGia
 {
     private int MaTacGia;
     private String TenTacGia;
-    private String GhiChuTG;
+    private String GhiChu;
+
+    public TacGia(int MaTacGia) {
+        this.MaTacGia = MaTacGia;
+    }
+
+    public TacGia() {
+    }
+
+    public TacGia(int MaTacGia, String TenTacGia, String GhiChuTG) {
+        this.MaTacGia = MaTacGia;
+        this.TenTacGia = TenTacGia;
+        this.GhiChu = GhiChuTG;
+    }
+
+ 
+
+    public int getMaTacGia() {
+        return MaTacGia;
+    }
+
+    public String getTenTacGia() {
+        return TenTacGia;
+    }
+
+    public void setMaTacGia(int MaTacGia) {
+        this.MaTacGia = MaTacGia;
+    }
+
+    public void setTenTacGia(String TenTacGia) {
+        this.TenTacGia = TenTacGia;
+    }
+
+    public void setGhiChuTG(String GhiChuTG) {
+        this.GhiChu = GhiChuTG;
+    }
+    
+
+    public String getGhiChuTG() {
+        return GhiChu;
+    }
+    
 }
